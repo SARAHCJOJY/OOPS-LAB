9cefdcf170e5ffde10aa28a6d9d937147d649072
SARAHCJOJY <77264123+SARAHCJOJY@users.noreply.github.com>
2021-09-30 07:30:56 +0530

    Add files via upload

--- CopyFile.java
+++ CopyFile.java
@@ -4,8 +4,8 @@ class CopyFile
  public static void main(String args[])throws IOException, FileNotFoundException
   {
     int i;
-     File f1=new File("D:\OOPS\myname.txt");
-     File f2=new File("D:\OOPS\newone.txt");
+     File f1=new File("D:\\OOPS\\File1.txt");
+     File f2=new File("D:\\OOPS\\File2.txt");
      FileInputStream fin=new FileInputStream(f1);
      FileOutputStream fout=new FileOutputStream(f2);
      do
